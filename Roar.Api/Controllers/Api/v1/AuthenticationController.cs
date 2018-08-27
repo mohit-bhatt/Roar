249ca740a48a1d9c1a84ccd5dde985394111a5a3
Mohit Bhatt <mbhatt@paycor.com>
2018-08-27 13:05:06 -0500

    added authandparse

--- Roar.Api/Controllers/Api/v1/AuthenticationController.cs
+++ Roar.Api/Controllers/Api/v1/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Roar.Api.Models;
+using Roar.Api.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,5 +12,24 @@ namespace Roar.Api.Controllers.Api
     [Route("api/v1/Authentication")]
     public class AuthenticationController : ApiController
     {
+        [HttpPost]
+        [Route("authandparse")]
+        public HttpResponseMessage AuthenticateAndParse(EnrollmentModel enrollmentModel)
+        {
+            if((enrollmentModel == null) || (enrollmentModel.UserDetails == null))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid enrollment model provided");
+            }
+
+            VoiceItWrapper wrapper = new VoiceItWrapper();
+
+            var result = wrapper.authenticationByWavURL(enrollmentModel.UserDetails.UserId, enrollmentModel.UserDetails.Password, enrollmentModel.VoiceDataUrl);
+
+            if (string.IsNullOrEmpty(result))
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "The voice auth server did not respond back with a result");
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
     }
 }
