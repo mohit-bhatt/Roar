75ac4f259b96cd3388f75cd3c2723910510da6c9
Anupam Chopra <achopra@paycor.com>
2018-08-28 18:03:02 -0500

    Model for cosmos

---
+++ Roar.Api/Models/EmployeeEnrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Roar.Api.Models
+{
+    public class EmployeeEnrollment
+    {
+        public string EnrollmentId { get; set; }
+        public long EmployeeUid { get; set; }
+        public int ClientId { get; set; }
+    }
+}
